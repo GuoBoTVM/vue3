f65e3cb321d7c44e758f79a9bb1b37243c9216f0
guobo <guobo@wdcloud.cc>
2021-07-14 16:58:06 +0800

    1

---
+++ src/components/setup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>setup的调用</div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {},
+  setup(props) {
+    console.log(props.value);
+    //setup的调用在data,computed,methods,created被解析之前
+  },
+  created(){
+    console.log("123")
+  },
+};
+</script>
+
+<style>
+</style>
